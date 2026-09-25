608b8505eb1748440b326c5037143efe7565d622
SujoyU1 <164862857+SujoyU1@users.noreply.github.com>
2026-09-25 23:29:55 +0000

    Update: Cohort Retention: First Login and Consecutive-Day Logins

--- problems/1119-cohort-retention-first-login-and-consecutive-day-logins/solution.sql
+++ problems/1119-cohort-retention-first-login-and-consecutive-day-logins/solution.sql
@@ -1,25 +1,11 @@
-WITH table1 AS (
-    SELECT
-        user_id,
-        login_date,
-        LAG(login_date) OVER (
-            PARTITION BY user_id
-            ORDER BY login_date
-        ) AS lag_date
-    FROM logins
-),
+-- your query
+with table1 as(
+SELECT user_id,login_date,
+lag(login_date)over(partition by user_id order by login_date) as lag_date
+from logins),
+table2 as (
+select distinct user_id from table1 where login_date-lag_date=1)
+select table1.user_id, min(table1.login_date) as first_login
+from table1 inner join table2 on table1.user_id=table2.user_id group by table1.user_id 
 
-table2 AS (
-    SELECT DISTINCT user_id
-    FROM table1
-    WHERE login_date - lag_date = 1
-)
 
-SELECT
-    table1.user_id,
-    MIN(table1.login_date) AS first_login
-FROM table1
-INNER JOIN table2
-    ON table1.user_id = table2.user_id
-GROUP BY table1.user_id
-ORDER BY table1.user_id ASC;
