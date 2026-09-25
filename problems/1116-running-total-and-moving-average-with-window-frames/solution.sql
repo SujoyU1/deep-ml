c0c2751730e3bd98fac1a3ac7d40680fddbffc05
SujoyU1 <164862857+SujoyU1@users.noreply.github.com>
2026-09-25 18:57:09 +0000

    Update: Running Total and Moving Average with Window Frames

--- problems/1116-running-total-and-moving-average-with-window-frames/solution.sql
+++ problems/1116-running-total-and-moving-average-with-window-frames/solution.sql
@@ -1,6 +1,6 @@
 -- your query
 SELECT 
 day,amount,sum(amount) over(order by day asc) as running_total, 
-avg(amount) over(order by day ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) as moving_avg
+avg(amount) over(order by day asc ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) as moving_avg
 from sales 
 order by day asc
